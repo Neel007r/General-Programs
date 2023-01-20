52df7e94ed6df52e6871eea6e7286104d65037a0
Neel <neelrpatel007@gmail.com>
2023-01-20 18:20:48 -0500

    fourth commit

---
+++ continue_in_whileloop.java
@@ -0,0 +1,31 @@
+package Practice;
+
+public class continue_in_whileloop {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		while(i<=10) {
+			if(i==5) {
+				i++;
+				continue;
+			}
+			System.out.println(i);
+			i++;
+		}
+//		for(int i=1;i<=3;i++) {
+//			
+//			for(int j=1;j<=3;j++) {
+//				
+//				if(i==2&&j==2) {
+//				continue;
+//			}
+//			System.out.println(i+" "+j);
+//			
+
+
+   
+    	   
+        
+   }
+}
